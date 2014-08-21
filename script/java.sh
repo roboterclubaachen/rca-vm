d9679117336e8d96c93ede64db8cb06aba6bf4ba
Electronic Kiwi <electron.kiwi@gmail.com>
2014-08-21 19:50:20 +0200

    try to use openjdk

--- script/java.sh
+++ script/java.sh
@@ -1,15 +1,1 @@
-# add required repositories
-sudo apt-get install -y software-properties-common
-add-apt-repository ppa:webupd8team/java
-apt-get -y update
-
-# install oracles java7
-
-# this silences the license question
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
-sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-sudo debconf-set-selections
-
-sudo apt-get install -y oracle-java7-installer librxtx-java
-sudo ln -s /usr/lib/jvm/java-7-oracle/ /usr/lib/jvm/default-java
+sudo apt-get install -y openjdk-7-jdk librxtx-java
