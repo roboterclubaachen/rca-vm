a8440abe0630685baffd123413fee8c8e19759cf
Electronic Kiwi <electron.kiwi@gmail.com>
2014-08-21 19:51:17 +0200

    use minimal xfce desktop
    this tries to stay as close to the xubuntu default
    as possible

---
+++ script/tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Some Basic Tools:
+# Terminal Emulator: xfce4-terminal
+# Picture-Viewer: ristretto
+# Browser: midori
+
+
+apt-get install -y xfce4-terminal ristretto midori
---
+++ script/xfce.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "==> Installing xfce"
+apt-get install -y xfce4 slim xubuntu-icon-theme shimmer-themes xfce4-places-plugin
+
+# file manager plugins and better program menu
+apt-get install -y thunar-archive-plugin thunar-media-tags-plugin xfce4-whiskermenu-plugin
+
+# move backround image to backgrounds directory
+mv wallpaper_captain.jpg /usr/share/backgrounds/xfce/
+
+USERNAME=rca
+SLIM_CONFIG=/etc/slim.conf
+
+echo "# RCA settings #" >> $SLIM_CONFIG
+echo "################" >> $SLIM_CONFIG
+echo "numlock on" >> $SLIM_CONFIG
+echo "default_user ${USERNAME}" >> $SLIM_CONFIG
+echo "auto_login yes" >> $SLIM_CONFIG
