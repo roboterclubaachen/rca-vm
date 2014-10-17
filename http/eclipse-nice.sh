67b85c0670a4b6d7eaa5489f9aac97752f6312ee
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-17 23:52:06 +0200

    Add default workspace to eclipse.

--- http/eclipse-nice.sh
+++ http/eclipse-nice.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-GTK2_RC_FILES=$GTK2_RC_FILES:/opt/eclipse/eclipse.gtkrc /opt/eclipse/eclipse
+GTK2_RC_FILES=$GTK2_RC_FILES:/opt/eclipse/eclipse.gtkrc /opt/eclipse/eclipse -vmargs -Dosgi.instance.area.default=@user.home/rcaworkspaces/workspace15
