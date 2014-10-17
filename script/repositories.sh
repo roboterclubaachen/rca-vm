7f5be547f4e4baf144fbf9c8f8f266116c0be2c5
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-18 01:19:31 +0200

    Make gitslave not use a pager.

--- script/repositories.sh
+++ script/repositories.sh
@@ -10,17 +10,17 @@ mkdir ~/rcasoftware
 
 echo
 echo 'Cloning last seasons software repositories...'
-gits clone https://$RCA_USER:$RCA_PASSWORD@$RCA_REPOSITORY_PATH/git/software.git -b season2014 ~/rcasoftware/s2014
+gits --no-pager clone https://$RCA_USER:$RCA_PASSWORD@$RCA_REPOSITORY_PATH/git/software.git -b season2014 ~/rcasoftware/s2014
 
 echo
 echo 'Cloning the current software repositories...'
-gits clone https://$RCA_REPOSITORY_PATH/git/software.git -b develop ~/rcasoftware/s2015
+gits --no-pager clone https://$RCA_REPOSITORY_PATH/git/software.git -b develop ~/rcasoftware/s2015
 
 echo
 echo 'Setting up git-flow...'
 cd ~/rcasoftware/s2015/
-gits exec git branch --track master origin/master
-gits exec git flow init -d
+gits --no-pager exec git branch --track master origin/master
+gits --no-pager exec git flow init -d
 
 echo
 echo 'Copying eclipse configuration...'
