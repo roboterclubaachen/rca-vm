0ae9b3fed5998589b0434cafdd283ff39b30657a
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-18 01:48:42 +0200

    Really fix pager for gitslave. Add symlink to desktop for rcasoftware.

--- script/repositories.sh
+++ script/repositories.sh
@@ -3,6 +3,8 @@
 export RCA_USER=censored
 export RCA_PASSWORD=censored
 export RCA_REPOSITORY_PATH=censored
+export PAGER=cat
+export GIT_PAGER=cat
 git config --global credential.https://$RCA_REPOSITORY_PATH.helper store
 
 cd
@@ -10,17 +12,17 @@ mkdir ~/rcasoftware
 
 echo
 echo 'Cloning last seasons software repositories...'
-gits --no-pager clone https://$RCA_USER:$RCA_PASSWORD@$RCA_REPOSITORY_PATH/git/software.git -b season2014 ~/rcasoftware/s2014
+gits clone https://$RCA_USER:$RCA_PASSWORD@$RCA_REPOSITORY_PATH/git/software.git -b season2014 ~/rcasoftware/s2014
 
 echo
 echo 'Cloning the current software repositories...'
-gits --no-pager clone https://$RCA_REPOSITORY_PATH/git/software.git -b develop ~/rcasoftware/s2015
+gits clone https://$RCA_REPOSITORY_PATH/git/software.git -b develop ~/rcasoftware/s2015
 
 echo
 echo 'Setting up git-flow...'
 cd ~/rcasoftware/s2015/
-gits --no-pager exec git branch --track master origin/master
-gits --no-pager exec git flow init -d
+gits exec git branch --track master origin/master
+gits exec git flow init -d
 
 echo
 echo 'Copying eclipse configuration...'
@@ -30,3 +32,5 @@ cd ~/rcasoftware/s2015/config/eclipse/
 cp -iRv . ~/rcasoftware/s2015/
 
 rm ~/.git-credentials
+# symlink to desktop for easier access
+ln -s ~/rcasoftware ~/Desktop/
