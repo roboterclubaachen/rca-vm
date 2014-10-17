93b16fc1053b5c6e7053564729e447147e263393
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-18 00:30:00 +0200

    Update Eclipse to Luna release.

--- script/eclipse.sh
+++ script/eclipse.sh
@@ -1,7 +1,7 @@
 VAGRANT_USER=rca
 
 echo "==> download and expand eclipse"
-curl http://ftp.halifax.rwth-aachen.de/eclipse/technology/epp/downloads/release/kepler/SR2/eclipse-cpp-kepler-SR2-linux-gtk-x86_64.tar.gz \
+curl http://ftp.halifax.rwth-aachen.de/eclipse//technology/epp/downloads/release/luna/SR1/eclipse-cpp-luna-SR1-linux-gtk-x86_64.tar.gz \
     -o eclipse.tar.gz
 tar -xvzf eclipse.tar.gz
 rm eclipse.tar.gz
@@ -12,16 +12,6 @@ mv ~/eclipse.desktop /usr/share/applications
 mv ~/eclipse-nice.sh /usr/bin
 mv ~/eclipse.gtkrc /opt/eclipse
 chmod a+x /usr/bin/eclipse-nice.sh
-
-# this is a really evil hack
-ECLIPSE_FOLDER_NAME=org.eclipse.platform_4.3.2_1473617060_linux_gtk_x86_64
-# I wish Eclipse was better scriptable...
-echo "==> set workspace manually"
-mkdir -p ~/.eclipse/$ECLIPSE_FOLDER_NAME
-cp -r /opt/eclipse/configuration ~/.eclipse/$ECLIPSE_FOLDER_NAME
-mkdir -p ~/.eclipse/$ECLIPSE_FOLDER_NAME/configuration/.settings
-mv ~/org.eclipse.ui.ide.prefs ~/.eclipse/$ECLIPSE_FOLDER_NAME/configuration/.settings
-chown -R rca ~/.eclipse
 
 # We are installing the following plug-ins:
 #   - Eclipse Java Development Tools
