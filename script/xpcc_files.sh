8b37f4e38c6f04cdbfbe1b696a589344b7962df5
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-18 23:40:35 +0200

    Reorder scripts and execute repositories without root.

--- script/xpcc_files.sh
+++ script/xpcc_files.sh
@@ -1,4 +1,3 @@
 # move these files to the right location
 mv ~/busblaster-ktlink.cfg /usr/share/openocd/scripts/interface/busblaster-ktlink.cfg
 mv ~/40-openocd.rules /etc/udev/rules.d/40-openocd.rules
-mv ~/gitconfig ~/.gitconfig
