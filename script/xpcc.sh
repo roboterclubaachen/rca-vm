6af88c851fba11a371ae2fe61e2d2109a86edaf9
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-10-28 16:36:12 +0100

    Install tree.

--- script/xpcc.sh
+++ script/xpcc.sh
@@ -14,7 +14,7 @@ git-flow gitk build-essential libboost-thread-dev libboost-system-dev meld \
 libasio-dev gcc-avr binutils-avr avr-libc pandoc avrdude curl gitg gcc texinfo \
 markdown python-docutils libusb-dev dfu-programmer bash-completion \
 libgtkmm-2.4-dev libreadline-dev libsdl1.2-dev libsdl-image1.2-dev git-core \
-subversion libusb-1.0-0-dev python-pip libftdi-dev libtool ack-grep
+subversion libusb-1.0-0-dev python-pip libftdi-dev libtool ack-grep tree
 
 apt-get build-dep -y dfu-util
 apt-get --no-install-recommends install -y doxygen
