cb19baca68cfee75e20ad7dcfd57adb4bd307c90
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-01-07 13:55:20 +0100

    Add openjdk-7-source fixes #34.

--- script/java.sh
+++ script/java.sh
@@ -1,2 +1,2 @@
-apt-get install -y openjdk-7-jdk librxtx-java libjinput-java
+apt-get install -y openjdk-7-source openjdk-7-jdk librxtx-java libjinput-java
 ln -s /usr/lib/jvm/java-7-openjdk-amd64 /usr/lib/jvm/default-java
