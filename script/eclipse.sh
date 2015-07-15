a665d29786340dd980ff390d8a69d97cee4c5455
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2015-07-15 02:18:07 +0200

    Fix eclipse installation.

--- script/eclipse.sh
+++ script/eclipse.sh
@@ -1,11 +1,10 @@
-echo "==> download and expand eclipse-installer"
+echo "==> download and expand eclipse-installer (oomph)"
 curl http://ftp.fau.de/eclipse/oomph/updates/latest/org.eclipse.oomph.site.zip -o oomph.zip
 mkdir ~/eclipse
 unzip oomph.zip -d ~/eclipse/eclipse-installer
 rm oomph.zip
 
-echo "==> installing eclipse to location"
-mv eclipse /opt
+echo "==> installing eclipse launch files"
 mv ~/eclipse.desktop /usr/share/applications
 mv ~/eclipse-nice.sh /usr/bin
 mv ~/eclipse.gtkrc /home/rca/eclipse/
